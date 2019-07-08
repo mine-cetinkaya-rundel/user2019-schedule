b56f9f130429707c5fca5efc1195467cd294dfdc
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2019-07-08 14:00:31 +0200

    Add webshots

---
+++ take-webshot.R
@@ -0,0 +1,12 @@
+webshot("https://connect.rstudioservices.com/content/331/user2019-schedule.html#wednesday", 
+        "webshots/shot-wed.png", 
+        vheight = 2200, vwidth = 1300)
+
+webshot("https://connect.rstudioservices.com/content/331/user2019-schedule.html#thursday", 
+        "webshots/shot-thu.png", 
+        vheight = 3500, vwidth = 1300)
+
+webshot("https://connect.rstudioservices.com/content/331/user2019-schedule.html#friday", 
+        "webshots/shot-fri.png", 
+        vheight = 2700, vwidth = 1300)
+
